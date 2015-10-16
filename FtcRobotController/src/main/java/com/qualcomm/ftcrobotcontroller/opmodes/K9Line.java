d0bf688852f0b7073a9851bcfc3e0c3f93d4b73e
U-PGHSCHOOL\stjjensen1 <stjjensen1@LT-306-JJENSEN1.pghschool.loc>
2015-10-16 10:50:31 -0400

    Automatically reformat files

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9Line.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9Line.java
@@ -44,7 +44,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class K9Line extends OpMode {
 	
 	final static double MOTOR_POWER = 0.15; // Higher values will cause the robot to move faster
-	final static double HOLD_IR_SIGNAL_STRENGTH = 0.20; // Higher values will cause the robot to follow closer
+	fi
+    l static double HOLD_IR_SIGNAL_STRENGTH = 0.20; // Higher values will cause the robot to follow closer
 	final static double LIGHT_THRESHOLD = 0.5;
 
 	double armPosition;
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testop1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testop1.java
@@ -1,27 +1,29 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by stjzito1 on 10/13/2015.
  */
-public class Testop1 extends OpMode{
+public class Testop1 extends OpMode {
     DcMotor motorone;
+
     public Testop1() {
 
     }
+
     public void init() {
 
     }
+
     public void loop() {
         float throttle = gamepad1.left_stick_y;
         motorone.setPower(throttle);
         telemetry.addData("2 Status", "running for " + runtime.toString());
 
     }
+
     public void stop() {
 
     }
