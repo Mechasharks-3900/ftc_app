b8f800521c20596c962ebc8b55bbb297b4f7a4b2
jz4396 <jz4396@gmail.com>
2015-10-16 11:18:52 -0400

    fix error  of runtime.tostring()

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testop1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Testop1.java
@@ -20,7 +20,6 @@ public class Testop1 extends OpMode {
     public void loop() {
         float throttle = gamepad1.left_stick_y;
         motorone.setPower(throttle);
-        telemetry.addData("2 Status", "running for " + runtime.toString());
 
     }
 
