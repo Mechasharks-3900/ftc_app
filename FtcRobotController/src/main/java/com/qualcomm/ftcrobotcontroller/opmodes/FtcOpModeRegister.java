39aeabc635283063f6301bb53686dc4b6a1078a9
jus1in <justinj10000@gmail.com>
2015-11-10 10:33:23 -0500

    Add Servo Test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,7 +56,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
         manager.register("TestOp1", Testop1.class);
-
+        manager.register("Servo Test", ServoTest.class);
         manager.register("NullOp", NullOp.class);
 
         manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
