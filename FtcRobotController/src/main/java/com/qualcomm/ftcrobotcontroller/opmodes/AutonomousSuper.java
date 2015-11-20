df1c6412bbddf6e494e5ecaa88233afeb5574d96
jz4396 <jz4396@gmail.com>
2015-11-20 11:53:50 -0500

    lyhkv

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousSuper.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousSuper.java
@@ -9,21 +9,22 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
  * Created by stjzito1 on 11/17/2015.
  */
 public  abstract class AutonomousSuper extends OpMode {
-    public abstract void loop();
-    public abstract void init();
+    //public abstract void loop();
+    //public abstract void init();
     DcMotor motorLeft, motorRight;
+
     public void resetEncoder(){
         motorLeft.getController().setMotorChannelMode(1,DcMotorController.RunMode.RESET_ENCODERS);
-        motorRight.getController().setMotorChannelMode(1,DcMotorController.RunMode.RESET_ENCODERS);
+        motorRight.getController().setMotorChannelMode(2,DcMotorController.RunMode.RESET_ENCODERS);
     }
 
-    public void drive(int distance, int speed) {
+    public void driveForward(int distance, int speed) {
         int rightposition = motorRight.getCurrentPosition();
         int leftposition = motorLeft.getCurrentPosition();
         motorRight.setTargetPosition(distance - rightposition);
         motorLeft.setTargetPosition(distance - leftposition);
         motorLeft.getController().setMotorChannelMode(1, DcMotorController.RunMode.RUN_TO_POSITION);
-        motorRight.getController().setMotorChannelMode(1, DcMotorController.RunMode.RUN_TO_POSITION);
+        motorRight.getController().setMotorChannelMode(2, DcMotorController.RunMode.RUN_TO_POSITION);
         //motorRight.setPower(speed);
         //motorLeft.setPower(speed);
     }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousTest1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousTest1.java
@@ -8,12 +8,16 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
  * Created by stjzito1 on 11/17/2015.
  */
 public class AutonomousTest1 extends AutonomousSuper{
+
+
     public void init(){
-        resetEncoder();
-        drive(10,100);
+        motorLeft = hardwareMap.dcMotor.get("motor left");
+        motorRight = hardwareMap.dcMotor.get("motor right");
     }
 
     public void loop(){
         //does nothing
-   }
+        driveForward(100,100);
+        telemetry.addData("Right Encoder", motorRight.getCurrentPosition());
+    }
 }
