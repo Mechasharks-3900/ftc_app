b3e697b3ffcd0fa7341f6e959ecdb04aa503d2a9
jus1in <justinj10000@gmail.com>
2015-12-02 08:32:45 -0500

    Add Gamepad and Motor Test opmodes

--- FtcRobotController/src/main/java/com/mechasharks/opmodes/GamepadTest.java
+++ FtcRobotController/src/main/java/com/mechasharks/opmodes/GamepadTest.java
@@ -1,19 +1,53 @@
 package com.mechasharks.opmodes;
 
+import com.mechasharks.Register;
+import com.mechasharks.gamepad.WrappedGamepad;
 import com.mechasharks.opmodes.abstractmodes.TeleOp;
 
 /**
  * Created by stjjensen1 on 12/1/2015.
  */
+@Register(name = "Gamepad Test")
 public class GamepadTest extends TeleOp {
+
+    WrappedGamepad pad;
+
+    @Override
+    public void start() {
+        super.start();
+        pad = gamepad1;
+    }
 
     @Override
     public void loop() {
+        telemetry.addData("Current Controller", pad == gamepad1 ? "gamepad 1" : "gamepad 2");
         telemetry.addData("Button States",
                 String.format("A: %b, B: %b, X: %b, Y: %b",
-                        gamepad1.a(),
-                        gamepad1.b(),
-                        gamepad1.x(),
-                        gamepad1.y()));
+                        pad.a(),
+                        pad.b(),
+                        pad.x(),
+                        pad.y()
+                ));
+        telemetry.addData("DPad",
+                String.format("Right: %b, Up: %b, Left: %b, Down: %b",
+                        pad.dpad.right(),
+                        pad.dpad.up(),
+                        pad.dpad.left(),
+                        pad.dpad.down()
+                ));
+        telemetry.addData("Joysticks",
+                String.format("X1: %f, Y1: %f, B1: %b, X2: %f, Y2: %f, B2: %b",
+                        pad.leftStick.getX(),
+                        pad.leftStick.getY(),
+                        pad.leftStick.button(),
+                        pad.rightStick.getX(),
+                        pad.rightStick.getY(),
+                        pad.rightStick.button()
+                ));
+
+        if (pad == gamepad1 && gamepad2.a())
+            pad = gamepad2;
+        else if (pad == gamepad2 && gamepad1.a())
+            pad = gamepad1;
     }
 }
--- FtcRobotController/src/main/java/com/mechasharks/opmodes/MotorTest.java
+++ FtcRobotController/src/main/java/com/mechasharks/opmodes/MotorTest.java
@@ -1,7 +1,55 @@
 package com.mechasharks.opmodes;
 
+import com.mechasharks.Register;
+import com.mechasharks.opmodes.abstractmodes.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by stjjensen1 on 12/1/2015.
  */
-public class MotorTest {
+@Register(name = "Motor Test")
+public class MotorTest extends TeleOp {
+
+    List<Map.Entry<String,DcMotor>> motors;
+
+    @Override
+    public void start() {
+        super.start();
+        motors = new ArrayList<>(hardwareMap.dcMotor.entrySet());
+    }
+
+    int index = 0;
+    double power = 0;
+    boolean upPressed, downPressed;
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad.right()) {
+            power += 1.0 / 64;
+        }
+        if (gamepad1.dpad.left()) {
+            power -= 1.0 / 64;
+        }
+        int newIndex = index;
+        if (upPressed != gamepad1.dpad.up()) {
+            if (gamepad1.dpad.up()) {
+                power = 0;
+                newIndex = (index + motors.size() - 1) % motors.size();
+            }
+            upPressed = gamepad1.dpad.up();
+        }
+        if (downPressed != gamepad1.dpad.down()) {
+            if (gamepad1.dpad.down()) {
+                power = 0;
+                newIndex = (index + 1) % motors.size();
+            }
+            downPressed = gamepad1.dpad.down();
+        }
+        motors.get(index).getValue().setPower(power);
+        index = newIndex;
+    }
 }
