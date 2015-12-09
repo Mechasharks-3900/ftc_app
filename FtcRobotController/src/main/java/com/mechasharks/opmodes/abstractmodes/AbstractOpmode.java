5e21f60ab60a6ea0488f0a65b1ae8532e537561f
jus1in <justinj10000@gmail.com>
2015-12-09 08:10:12 -0500

    Add support for front motors

--- FtcRobotController/src/main/java/com/mechasharks/opmodes/abstractmodes/AbstractOpmode.java
+++ FtcRobotController/src/main/java/com/mechasharks/opmodes/abstractmodes/AbstractOpmode.java
@@ -8,26 +8,30 @@ import com.qualcomm.robotcore.hardware.DcMotorController.RunMode;
  * Created by stjjensen1 on 11/20/2015.
  */
 public abstract class AbstractOpMode extends OpMode {
-    protected DcMotor driveLeft, driveRight;
+    protected DcMotor driveLeft, driveRight, driveFrontLeft, driveFrontRight;
 
 
     @Override
     public void init() {
         driveLeft = hardwareMap.dcMotor.get("motor left");
         driveRight = hardwareMap.dcMotor.get("motor right");
+        driveFrontLeft = hardwareMap.dcMotor.get("motor front left");
+        driveFrontRight = hardwareMap.dcMotor.get("motor front right");
         resetEncoders();
     }
 
     public void drive(double left, double right) {
-        driveLeft.setChannelMode(RunMode.RUN_WITHOUT_ENCODERS);
-        driveLeft.setChannelMode(RunMode.RUN_WITHOUT_ENCODERS);
-        driveLeft.setPower(left);
-        driveRight.setPower(right);
+        move(driveLeft, left);
+        move(driveRight, right);
+        move(driveFrontLeft, left);
+        move(driveFrontRight, right);
     }
 
     public void resetEncoders(){
         driveLeft.setChannelMode(RunMode.RESET_ENCODERS);
         driveRight.setChannelMode(RunMode.RESET_ENCODERS);
+        driveFrontLeft.setChannelMode(RunMode.RESET_ENCODERS);
+        driveFrontRight.setChannelMode(RunMode.RESET_ENCODERS);
     }
 
     public void driveTo(int position, float power) {
@@ -36,6 +40,8 @@ public abstract class AbstractOpMode extends OpMode {
 
     public void driveTo(int position, float power, boolean relative) {
         moveTo(driveLeft, position, power, relative);
+        moveTo(driveFrontLeft, position, power, relative);
+        moveTo(driveFrontRight, position, power, relative);
         moveTo(driveRight, position, power, relative);
     }
 
@@ -45,6 +51,11 @@ public abstract class AbstractOpMode extends OpMode {
             newVal += m.getCurrentPosition();
         m.setChannelMode(RunMode.RUN_TO_POSITION);
         m.setTargetPosition(newVal);
-        m.setPower(power/100);
+        m.setPower(power / 100);
+    }
+
+    public void move(DcMotor m, double power) {
+        m.setChannelMode(RunMode.RUN_WITHOUT_ENCODERS);
+        m.setPower(power);
     }
 }
